cd55401b8fa3fe055a443a561ce98efec033d2cb
debia <debia@WIN-4M0E9EVG3TK>
2015-05-07 16:19:40 -0430

    Yay!!!

--- Tarea2/Billetera.py
+++ Tarea2/Billetera.py
@@ -9,63 +9,99 @@ Created on 29/4/2015
 from datetime import datetime
 from datetime import timedelta
 from decimal import Decimal
+from abc import ABCMeta
 import hashlib
 import uuid
+from builtins import str
+
+class ZeroException(Exception):
+    pass
+
+class NegativeException(Exception):
+    pass
+
+class NoFundsException(Exception):
+    pass
+
+class WrongPINException(Exception):
+    pass
+
+class VoidException(Exception):
+    pass
+
 
 class Usuario(object):
+    
+    __metaclass__ = ABCMeta
+    
     def __init__(self, nombres, apellidos, CI):
         self.nombres = nombres
         self.apellidos = apellidos
         self.CI = CI
 
-class Transaccion(object):
-    def __init__(self,monto,fecha,ID_Lugar,tipo):
+class Debito(object):
+    def __init__(self,monto,fecha,ID_Lugar):
         self.monto = monto
         self.fecha = fecha
         self.ID_Lugar = ID_Lugar
-        self.tipo = tipo #1=credito, 0=debito
+    
+    def getMonto(self):
+        return self.monto
+        
+class Credito(object):
+    def __init__(self,monto,fecha,ID_Lugar):
+        self.monto = monto
+        self.fecha = fecha
+        self.ID_Lugar = ID_Lugar
+    
+    def getMonto(self):
+        return self.monto
                 
 class BilleteraElectronica(object):
     #=)
-        def __init__(self, identificador,usuario,PIN):
+        def __init__(self, identificador, nombreUsuario, apellidoUsuario, ciUsuario, PIN):
             self.identificador = identificador
-            self.usuario = usuario
+            
+            if ((nombreUsuario == "" or apellidoUsuario == "" or PIN == "") or
+            not(isinstance(nombreUsuario, str)) or
+            not(isinstance(apellidoUsuario, str)) or
+            not(isinstance(PIN, str))) :
+                raise VoidException
+            
+            self.nombreUsuario = nombreUsuario
+            self.apellidoUsuario = apellidoUsuario
+            self.ciUsuario = ciUsuario
             
             salt = uuid.uuid4().hex
             self.PIN = hashlib.sha256(salt.encode() + PIN.encode()).hexdigest() + ':' + salt
             
-            self.transacciones = []
+            self.debitos = []
+            self.creditos = []
             self.saldo_actual = Decimal(0)
             
         def saldo(self):
             return self.saldo_actual
         
         def recargar(self,monto,fecha,ID_Lugar): 
-            monto = Decimal(monto)
-            self.transacciones.append(Transaccion(monto, fecha, ID_Lugar, 1))
-            self.saldo_actual += monto
+            if monto == 0:
+                raise ZeroException
+            if monto <= 0:
+                raise NegativeException
+            
+            self.creditos += [Credito(Decimal(monto), fecha, ID_Lugar)]                   
+            self.saldo_actual += Decimal(monto)
             
         def consumir(self,monto,fecha,ID_Lugar,PIN):
+            if monto == 0:
+                raise ZeroException
+            elif monto < 0:
+                raise NegativeException
+            elif Decimal(monto) > self.saldo_actual:
+                raise NoFundsException
             
             PIN_prueba, salt = self.PIN.split(':')
             if PIN_prueba != hashlib.sha256(salt.encode() + PIN.encode()).hexdigest():
-                return "Su PIN no coincide con el introducido"
-            if self.saldo_actual < monto:
-                return "No tiene suficiente saldo"
+                raise WrongPINException
             
-            monto = Decimal(monto)
-            self.transacciones.append(Transaccion(monto, fecha, ID_Lugar, 0))
-            self.saldo_actual -= monto
-            
-        
-            
-            
-
-            
-a = BilleteraElectronica(1,Usuario("pedrño","pérez",23712077), "123456")
-
-a.recargar(2, datetime(year=1,month=1,day=1), "1")
-a.recargar(10, datetime(year=1,month=1,day=1), "1")
-print(a.consumir(13, datetime(year=1,month=1,day=1), "2", "123456"))
-print(a.saldo())
-
+            self.debitos += [Debito(Decimal(monto), fecha, ID_Lugar)]
+            self.saldo_actual -= Decimal(monto).quantize(Decimal('1.00'))
--- Tarea2/testBilletera.py
+++ Tarea2/testBilletera.py
@@ -14,87 +14,185 @@ import uuid
 import unittest
 from Billetera import * 
 import Billetera
+import xdrlib
 
 
-class Test(unittest.TestCase):
-
-  
-
-    def testSaldo1(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")      
-        self.assertEqual(a.saldo(),a.saldo_actual)
+class TestBilleteraExists(unittest.TestCase):
+    
+    def testBilleteraExists(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        
+    def testBilleteraCaracterRaro(self):
+        a = BilleteraElectronica(1, "Vïrçíñî@", "$th&lê <3", 23712077, "123456")
+        self.assertEqual(a.nombreUsuario + a.apellidoUsuario, "Vïrçíñî@" + "$th&lê <3")
+    
+    def testNombreVacio(self):    
+        self.assertRaises(VoidException, BilleteraElectronica,1,"","",23631926,"243")
+    
+    def testNombreNull(self):    
+        self.assertRaises(VoidException, BilleteraElectronica,1,None,None,23631926,"243")
+    
+    def testNombreNoString(self):    
+        self.assertRaises(VoidException, BilleteraElectronica,1,123,Decimal(0.5),23631926,"243")
+        
+    def testPINVacio(self):
+        self.assertRaises(VoidException, BilleteraElectronica,1,"pedro","perez",23631926,"")
+    
+    def testPINNulo(self):
+        self.assertRaises(VoidException, BilleteraElectronica,1,"pedro","perez",23631926,None)
+       
+    def testSaldoEsDecimal(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        self.assertTrue(isinstance(a.saldo(), Decimal))
     
     def testRecargaDesdeCero(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-        a.recargar(1, datetime(year =1, month =1, day =1), 1)
-        self.assertEqual(a.saldo(), 1)
-    
-    '''def testRecargaVacia(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-        a.recargar(0, datetime(year =1, month =1, day =1), 1)
-        self.assertRaisesRegex(Exception, )'''
-        
-    '''def testRecargaNegativa(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-        a.recargar(-1, None, None)
-        self.assertEqual(a.saldo, 0)'''
-        
-    def testRecargaMuyAltaDesde0(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-        a.recargar(2**31, None, None)
-        self.assertEqual(a.saldo(), 2**31)
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(10, fecha, 1)
+        self.assertEqual(a.saldo(), 10)
         
     def testRecargaDesdeNoCero(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-        a.recargar(10, None, None)
-        a.recargar(10, None, None)
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(10, fecha, 1)
+        a.recargar(10, fecha, 1)
         self.assertEqual(a.saldo(), 20)
+    
+    def testRecargaVacia(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        self.assertRaises(ZeroException, a.recargar, 0, fecha, 1)
         
-    def testRecargaMuyAltaDesdeNo0(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-        a.recargar(1, None, None)
-        a.recargar((2**31)-1, None, None)
+    def testRecargaNegativa(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")    
+        fecha = datetime(year = 1, month = 1, day = 1)    
+        self.assertRaises(NegativeException, a.recargar, -1, fecha, 1)
+        
+    def testRecargaMuyAltaDesde0(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1) 
+        a.recargar(2**31, fecha, 1)
         self.assertEqual(a.saldo(), 2**31)
         
+    def testRecargaMuyAltaDesdeNo0(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1) 
+        a.recargar(1, fecha, 1)
+        a.recargar((2**31), fecha, 1)
+        self.assertEqual(a.saldo(), 2**31 + 1)
     
-    #Lista de pendientes:
-    
-    def testConsumoVacio(self):
-        pass
-    
-    #EN ESTE UNIVERSO SE PUEDE CONSUMIR 0    
-    def testConsumir0(self):
-        a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-        a.recargar(1, None, None)
-        a.consumir(0, None, None,"123456")
-        self.assertEqual(a.saldo(), 1)
+    def testRecargaDecimalBajaDesde0(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1) 
+        a.recargar(0.01, fecha, 1)
+        self.assertAlmostEqual(a.saldo(), Decimal(0.01), 2)
+        
+    def testRecargaDecimalBajaDesdeNo0(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(10, fecha, 1)
+        a.recargar(0.01, fecha, 1)
+        self.assertAlmostEqual(a.saldo(), Decimal(10.01), 2)
+        
+    def testCreditoAgregado(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        self.assertEqual(a.creditos, [])
+        a.recargar(10, fecha, 1)
+        self.assertEqual(a.creditos[-1].monto, Decimal(10))
+        self.assertEqual(a.creditos[-1].fecha, fecha)
+        self.assertEqual(a.creditos[-1].ID_Lugar, 1)
+        self.assertEqual(len(a.creditos), 1)
+        
+    def testCreditoAgregadoMultiple(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        self.assertEqual(a.creditos, [])
+        a.recargar(10, fecha, 1)
+        a.recargar(2**31, fecha, 2)
+        self.assertEqual(a.creditos[-1].monto, Decimal(2**31))
+        self.assertEqual(a.creditos[-1].fecha, fecha)
+        self.assertEqual(a.creditos[-1].ID_Lugar, 2)
+        self.assertEqual(len(a.creditos), 2)
+
+    def testConsumo0(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        self.assertRaises(ZeroException, a.consumir, 0, fecha, 1, "123456")
         
     def testConsumoNegativo(self):
-        pass
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        self.assertRaises(NegativeException, a.consumir, -1, fecha, 1, "123456")
     
     def testConsumoRegular(self):
-        pass
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(10, fecha, 1)
+        a.consumir(5, fecha, 1, "123456")
+        self.assertEquals(a.saldo(), Decimal(5))
+        
+    def testConsumoDecimal(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(10, fecha, 1)
+        a.consumir(2.01, fecha, 1, "123456")
+        self.assertAlmostEqual(a.saldo(), Decimal(7.99), 2)
+        
+    def testConsumoSaldoInsuficiente(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(1, fecha, 1)
+        self.assertRaises(NoFundsException, a.consumir, 2, fecha, 1, "123456")
+     
+    def testConsumoSaldoInsuficienteMargenMinimo(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(1, fecha, 1)
+        self.assertRaises(NoFundsException, a.consumir, 1.01, fecha, 1, "123456")
     
     def testConsumoPINErroneo(self):
-        pass
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(10, fecha, 1)
+        self.assertRaises(WrongPINException, a.consumir, 5, fecha, 1, "12345")
     
-    def testConsumoSaldoInsuficiente(self):
-        pass
+    def testConsumoMuyAlto(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(2**31, fecha, 1)
+        a.consumir(2**31, fecha, 1, "123456")
+        self.assertEquals(a.saldo(), Decimal(0))
     
-    def testConsumoCantidadMuyAlta(self):
-        pass
-    
-    
+    def testConsumoDecimalMuyAlto(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        a.recargar(2**31 + 0.01, fecha, 1)
+        a.consumir(2**31, fecha, 1, "123456")
+        self.assertAlmostEqual(a.saldo(), Decimal(0.01), 2)
         
+    def testDebitoAgregado(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        self.assertEqual(a.debitos, [])
+        a.recargar(10, fecha, 1)
+        a.consumir(10, fecha, 1, "123456")
+        self.assertEqual(a.debitos[-1].monto, Decimal(10))
+        self.assertEqual(a.debitos[-1].fecha, fecha)
+        self.assertEqual(a.debitos[-1].ID_Lugar, 1)
+        self.assertEqual(len(a.debitos), 1)
         
-    
-        
-    ''''a = BilleteraElectronica(1,Usuario("pedro","perez",23712077), "123456")
-
-a.recargar(2, datetime(year=1,month=1,day=1), "1")
-a.recargar(10, datetime(year=1,month=1,day=1), "1")
-print(a.consumir(13, datetime(year=1,month=1,day=1), "2", "123456"))
-print(a.saldo())'''
+    def testDebitoAgregadoMultiple(self):
+        a = BilleteraElectronica(1, "Virginia", "Gil", 23712077, "123456")
+        fecha = datetime(year = 1, month = 1, day = 1)
+        self.assertEqual(a.debitos, [])
+        a.recargar(10, fecha, 1)
+        a.consumir(5, fecha, 2, "123456")
+        a.consumir(5, fecha, 2, "123456")
+        self.assertEqual(a.debitos[-1].monto, Decimal(5))
+        self.assertEqual(a.debitos[-1].fecha, fecha)
+        self.assertEqual(a.debitos[-1].ID_Lugar, 2)
+        self.assertEqual(len(a.debitos), 2)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
